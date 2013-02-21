e3dabc40dd5458809c7080bd4f44ef92e6b5891a
kodamat <kt.kiyoshi@gmail.com>
2013-02-22 07:49:18 +0900

    change shell_env path

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#Move to target directory
+# Move to target directory
 cd /var/www/html/
 
-#Back up 
+# Back up 
 tar zcvf /home/kodamat/backup/backup_`date +%Y%m%d`.tgz *
 
-#Delete old one
-rm /home/kodamat/backup/backup_`date -d '1 days ago' +%Y%m%d`.tgz
+# Delete old one
+rm /home/kodamat/backup/backup_`date -d '7 days ago' +%Y%m%d`.tgz
 
-#Result
+# Result
 echo `date +%Y%m%d`' Finished!'
 cd /home/kodamat/backup/
 ls -lh
--- db_backup.sh
+++ db_backup.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-. ./shell_env
+# Read setting file
+. /home/kodamat/scripts/shell_env
+
 FILENAME=db_backup_`date +%Y%m%d`.sql
 
-#Back up 
+# Back up 
 mysqldump -u${USER} -p${PASS} _kiyoshi_wp > /home/kodamat/backup/${FILENAME}
 
-#Delete old one
-rm /home/kodamat/backup/db_backup_`date -d '1 days ago' +%Y%m%d`.sql
+# Delete old one
+rm /home/kodamat/backup/db_backup_`date -d '7 days ago' +%Y%m%d`.sql
 
-#Result
+# Result
 echo `date +%Y%m%d`' Finished!'
 cd /home/kodamat/backup/
 ls -lh
