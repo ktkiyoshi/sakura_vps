7928d6a5439cf44dbe26eec7e2c7f06f020634f2
kodamat <kt.kiyoshi@gmail.com>
2013-03-25 18:23:34 +0900

    modify script

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 cd /var/www/html/
 
 # Back up 
-tar zcvf /home/kodamat/backup/backup_`date +%Y%m%d`.tgz *
+tar zcf /home/kodamat/backup/backup_`date +%Y%m%d`.tgz *
 
 # Delete old one
 rm /home/kodamat/backup/backup_`date -d '7 days ago' +%Y%m%d`.tgz
